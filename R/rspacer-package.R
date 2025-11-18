73cd9f825bdeb234e7acb946623f5379c8cc7aa2
Gerhard Burger <burger.ga@gmail.com>
2025-11-18 15:31:08 +0100

    Update documentation
    * Explain tag option (closes #35)
    * Add info about community instance (closes #38)
    * Demonstrate simplest way to create document

--- R/rspacer-package.R
+++ R/rspacer-package.R
@@ -2,6 +2,13 @@
 #' @seealso [RSpace API Documentation](https://community.researchspace.com/public/apiDocs)
 #' @importFrom rlang .data
 #'
+#' @section Package options:
+#' The package uses the following options (set using `options()`):
+#' \describe{
+#'  \item{`rspacer.set_rspacer_tag`}{ Logical; if `TRUE`, will append the `rspacer`
+#'  tag to RSpace documents created or modified using `rspacer`. Default: `TRUE`.}
+#' }
+#'
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
